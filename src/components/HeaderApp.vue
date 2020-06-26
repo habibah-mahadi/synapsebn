5ba14ee5bd0962f4645abf8f4862530ee0736503
habibah-mahadi <habibah.hm701@gmail.com>
2020-06-26 20:06:18 +0800

    delete extra button

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -3,7 +3,6 @@
     <b-jumbotron header="BootstrapVue" lead="Bootstrap v4 Components for Vue.js 2">
       <p>For more information visit website</p>
       <b-button variant="primary" href="#">More Info</b-button>
-      <b-button variant="primary" href="#">More Info</b-button>
     </b-jumbotron>
   </div>
 </template>
